a4a755cdce3e4a478a664917ed4c2fdf45bc8085
youmin29 <babybana0829@naver.com>
2026-01-15 16:55:28 +0900

    ✨ Feat: Slack 이벤트 처리 라우터 추가

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from db.mongodb import connect_to_mongo
+from controllers.slack import router as SlackRouter
 from controllers.setting import router as SettingController
 from controllers.employee import router as EmployeeController
 
@@ -15,6 +16,6 @@ def startup():
 def root():
     return {"message": "Hello World"}
 
-
+app.include_router(SlackRouter)
 app.include_router(SettingController)
 app.include_router(EmployeeController)
